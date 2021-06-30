2f4c7de5f08f0297c5a7346c62c51fbac4d11750
sj50 <sameerjain.50@gmail.com>
2021-06-30 12:20:55 +1000

    added git-action CI/CD workflow file and cleanup master banch

--- javascript/api.js
+++
@@ -1,6 +0,0 @@
-var URL = "https://kaptotpr8c.execute-api.ap-southeast-2.amazonaws.com/Prod/Visitors-Count"
-fetch(URL)
-	.then(response => response.text())
-	.then((response) => {
-	document.getElementById("visits").innerHTML = response;
-	})
