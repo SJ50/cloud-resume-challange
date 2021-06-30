bea9532d6032a0d64becdfcafe84aa70ef9e27b7
sj50 <sameerjain.50@gmail.com>
2021-06-30 19:38:23 +1000

    updated api url

--- javascript/api.js
+++ javascript/api.js
@@ -1,5 +1,5 @@
-var URL = "https://kaptotpr8c.execute-api.ap-southeast-2.amazonaws.com/Prod/Visitors-Count"
-fetch(URL)
+var APIURL = "https://bwb1tlk55g.execute-api.ap-southeast-2.amazonaws.com/Prod/count/"
+fetch(APIURL)
 	.then(response => response.text())
 	.then((response) => {
 	document.getElementById("visits").innerHTML = response;
