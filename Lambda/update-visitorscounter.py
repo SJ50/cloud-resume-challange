1a76d5743d05e71d36d8be26cc8640d1dece569b
Sameer Jain <sameerjain.50@gmail.com>
2021-07-01 19:05:52 +1000

    Update update-visitorscounter.py
    added code to add extra information about page view in DynamoDB table.

--- Lambda/update-visitorscounter.py
+++ Lambda/update-visitorscounter.py
@@ -3,50 +3,64 @@ from decimal import Decimal
 import boto3
 
 # https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.Python.03.html
-
+ 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('VisitorsCountSAM')
-# update visitor count    
-    try:
-        response = table.update_item(
-            Key={
-                'SiteURL': 'sameerjain.net'
-            },
-            UpdateExpression="set visitorscount = visitorscount + :val",
-            ExpressionAttributeValues={
-                ':val': Decimal(1)
-            },
-            ReturnValues="UPDATED_NEW"
-        )
-    except Exception as e:
-        print("ClientError occured")
-        print("Adding record")
-        response = table.put_item(
-            Item={
-                'SiteURL': 'sameerjain.net',
-                'visitorscount': 1
-            }
-        )
-    else:
- #   return visitcount
+    # reqest country
+    country = event['headers']['CloudFront-Viewer-Country']
+    # request ip
+    ip = event['requestContext']['identity']['sourceIp']
+    # request time
+    lastvisittime = event['requestContext']['requestTime']
+    visitorscount = 1
+    # request user agent
+    #userAgent= event['requestContext']['identity']['userAgent']
+
+# update visitor visitorscount    
+    def updatecount(country, ip, visitorscount, lastvisittime):
+        try:
+            response = table.update_item(
+                Key={
+                    'Country': country,
+                    'IP': ip,
+                },
+                UpdateExpression="set VisitorsCount = VisitorsCount + :val",
+                ExpressionAttributeValues={
+                    ':val': Decimal(visitorscount)
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+            return response["Attributes"]["VisitorsCount"]
+        except Exception as e:
+            response = table.put_item(
+                Item={
+                    'Country': country,
+                    'IP': ip,
+                    'LastVisitTime': lastvisittime,
+                    'VisitorsCount': visitorscount
+                }
+            )
+            return visitorscount
+            
+    updatecount(country, ip, visitorscount, lastvisittime)
+    response = updatecount("ALL", "0.0.0.0", visitorscount, lastvisittime)
+    
  # https://hometechtime.com/how-to-return-multiple-attributes-from-dynomodb-via-a-lambda-function-and-api-gateway/
  # class DecimalEncoder to Resolve “errorMessage”: “Object of type Decimal is not JSON serializable”
-
-        class DecimalEncoder(json.JSONEncoder):
-            def default(self, obj):
-                if isinstance(obj, Decimal):
-                    return int(obj)
-                return json.JSONEncoder.default(self, obj)
+ #       class DecimalEncoder(json.JSONEncoder):
+ #           def default(self, obj):
+ #               if isinstance(obj, Decimal):
+ #                   return int(obj)
+ #               return json.JSONEncoder.default(self, obj)
  
  # headers to allow CORS
-
-        return {
-            "statusCode": 200,
-            'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET'
-            },
-            'body': json.dumps(response["Attributes"]["visitorscount"],cls=DecimalEncoder)
-        }
+    return {
+        "statusCode": 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET'
+        },
+        'body': json.dumps(int(response))
+    }
